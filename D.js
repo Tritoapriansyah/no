ccadf4b62241f818a83c241c7e509e230a524ce6
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2022-03-13 18:12:29 +0700

    Update D.js

--- D.js
+++ D.js
@@ -3533,7 +3533,7 @@ Ryuu.sendMessage(from, {${buttonsMessage},"buttons": ${buttons},
             "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=",
             "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC",
             "mediaKeyTimestamp": "1634472176",
-            "jpegThumbnail": fs.readFileSync(`./gambar/menu.jpg`)
+            "jpegThumbnail": mrpg
   }}, MessageType.buttonsMessage,{ quoted: ftroli, sendEphemeral: true, contextInfo:{"forwardingScore":999,"isForwarded":true, "externalAdReply":{"title": `Hai Kak ${pushname}`, "body": `Lolita Botz`, mediaType: 2, "thumbnailUrl": "https://cdn-image.hipwee.com/wp-content/uploads/2021/03/hipwee-gojo_satoru_by_chuutadesu_deb0dr7-fullview-750x422.jpg","previewType": "VIDEO","mediaUrl": `https://youtu.be/dQw4w9WgXcQ`}}})
   break
 //=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×==×=×=×=×=×=×=×=×=×==×=×=×=×=×=×=×=×=×==×=×=×=×=×=×=×=×=×=×			
